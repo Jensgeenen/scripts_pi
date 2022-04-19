8dbabca5eb705b053c938648aa5f0b414073f126
Jensgeenen <r0889619@student.thomasmore.be>
2022-04-19 13:35:31 +0200

    labo

--- lab5.3/practice_5.py
+++ lab5.3/practice_5.py
@@ -1,27 +1,29 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT) 
+
+GPIO.setup(17, GPIO.OUT)
 GPIO.setup(18, GPIO.IN)
+
 try:
     while(True):
         GPIO.output(17, 1)
-        time.sleep(0.00001) 
+        time.sleep(0.00001)
         GPIO.output(17, 0)
 
-    while(GPIO.input(18) == 0):
-        pass
-    
-    signalHigh = time.time()
+        while(GPIO.input(18) == 0):
+            pass
 
-    while(GPIO.input(18) == 1):
-        pass
-    
-    signallow = time.time()
-    timePassed = signalLow - signalHigh
-    distance= 17000 * timePassed
-    print(str(distance))
-    time.sleep(0.5)
+        signalHigh = time.time()
+
+        while(GPIO.input(18) == 1):
+            pass
+
+        signalLow = time.time()
+        timePassed = signalLow - signalHigh
+        distance = 17000 * timePassed
+        print(str(distance))
+        time.sleep(0.5)
 
 except KeyboardInterrupt:
     #cleanup
