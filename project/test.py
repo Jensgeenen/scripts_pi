2dc01e8d61d53a53a0c2a67e2d408dc9fb2095bc
Jensgeenen <r0889619@student.thomasmore.be>
2022-05-03 13:34:52 +0200

    visbak

---
+++ project/test.py
@@ -0,0 +1,49 @@
+import RPi.GPIO as GPIO
+import time
+teller = 0
+GPIO.cleanup ()
+# to use raspberry PI board GPIO numbers
+GPIO.setmode (GPIO.BCM)
+GPIO.setup (17, GPIO.IN) # GPIO 17 input
+GPIO.setup (18, GPIO.OUT) # GPIO 18 output
+GPIO.setup (23, GPIO.OUT)
+GPIO.setup (24, GPIO.OUT)
+GPIO.setup (25, GPIO.OUT)
+
+# loop forever or ctrl^C
+while True:
+    if (GPIO.input (17)==0):#input low active
+        if (teller < 150 ): 
+            print ("buttom pressed")
+            GPIO.output (18, 1)
+            GPIO.output (23, 0)
+            GPIO.output (24, 0)
+            GPIO.output (25, 0)    
+
+            time.sleep (0.005)
+
+            GPIO.output (18, 0)
+            GPIO.output (23, 1)
+            
+            time.sleep (0.005)
+            
+            GPIO.output (23, 0)
+            GPIO.output (24, 1)    
+
+            time.sleep (0.005)
+            
+            GPIO.output (24, 0)
+            GPIO.output (25, 1)
+
+            time.sleep (0.005)
+            teller += 1
+
+        else:
+            GPIO.output (18, 0)
+            GPIO.output (23, 0)
+            GPIO.output (24, 0)
+            GPIO.output (25, 0)   
+            print ("buttom release")
+            time.sleep (0.3) # anti bouncing
+    else: 
+        teller = 0       
